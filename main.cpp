2cf870be301daf01c4ce88766f038420fb3c2404
Silindokuhle15 <silindokuhle15sl@gmail.com>
2022-01-02 19:46:14 +0200

    Changed the Triangle into a square, sloppy method, drawing to triangle side by side...

--- main.cpp
+++ main.cpp
@@ -48,13 +48,24 @@ void setup_shader()
 
 void setup_data()
 {
-	float vertices[] = {
+	//float vertices[] = {
 		// For visualization: each row is a vertex.
 		// Each vertex has position [x, y, z] and color [r, g, b]
-		-0.5f, -0.5f, 0.0f, 1.0, 0.0, 0.0,   // red color for this vertex
-		 0.5f, -0.5f, 0.0f, 0.0, 1.0, 0.0,   // green color
-		 0.0f,  0.5f, 0.0f, 0.0, 0.0, 1.0    // blue color for our top vertex
+		//-0.5f, -0.5f, 0.0f, 1.0, 0.0, 0.0,   // red color for this vertex
+		//0.5f, -0.5f, 0.0f, 0.0, 1.0, 0.0,   // green color
+		// 0.5f,  0.5f, 0.0f, 0.0, 0.0, 1.0,   // blue color for our top vertex
+		//-0.5f,  0.5f, 0.0f, 1.0, 0.0, 0.0,	 // white color 
+		//-0.5f, -0.5f, 0.0f, 1.0, 0.0, 0.0   // red color for this vertex
+
+		float vertices[] = {
+		-0.5f,  0.5f, 0.0f, 1.0, 0.0, 0.0,  // top left point
+		 0.5f,  0.5f, 0.0f, 0.0, 1.0, 0.0,  // top right point
+		 0.5f, -0.5f, 0.0f, 0.0, 0.0, 1.0,  // bottom right point
+		 0.5f, -0.5f, 0.0f, 0.0, 0.0, 1.0,  // bottom right point
+		-0.5f, -0.5f, 0.0f, 1.0, 1.0, 1.0,  // bottom left point
+		-0.5f,  0.5f, 0.0f, 1.0, 0.0, 0.0   // top left point
 	};
+	//};
 
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
@@ -82,7 +93,7 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGL Triangle", NULL, 0);
+	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGL Square", NULL, 0);
 
 	if (!window)
 	{
@@ -101,13 +112,25 @@ int main(void)
 	setup_shader();
 	setup_data();
 
+	GLuint indices[] =
+	{
+		0,1,2,0,1
+	};
+	unsigned int indexBuffer = 0;
+	glGenBuffers(1, &indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), static_cast<const void*>(indices), GL_STATIC_DRAW);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader_program);
 		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+
+		//glDrawElements(GL_TRIANGLE_STRIP, 2, GL_UNSIGNED_SHORT, static_cast<void*>(indices));
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		glfwSwapBuffers(window);
 
